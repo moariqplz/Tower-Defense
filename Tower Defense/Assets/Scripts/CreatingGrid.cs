ebc65f585422138c160f3eb560a067f17344feab
moariqplz <shelbyk342@gmail.com>
2016-06-20 21:18:35 -0700

    Worked On: Creating the Grid Setting up the basic scene for level 1

---
+++ Tower Defense/Assets/Scripts/CreatingGrid.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatingGrid : MonoBehaviour {
+	public GameObject CellPrefab;
+	public Vector3 Size;
+	// Use this for initialization
+	void Start () 
+	{
+		CreateGrid ();	
+	}
+	void CreateGrid()
+	{
+		//Create the Grid Here
+		for (int x = -6; x < Size.x - 5; x++)
+		{
+			for (int y = -5; y < Size.y - 4; y++)
+			{
+				GameObject CellBlock = (GameObject)Instantiate (CellPrefab, new Vector3 (x, y, 0), CellPrefab.transform.rotation);
+				CellBlock.transform.parent = transform;
+			}
+		}
+	}
+}
