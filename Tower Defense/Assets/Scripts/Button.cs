9e15472abc2aed32cb08efeddf00832e782ef54e
moariqplz <shelbyk342@gmail.com>
2016-06-26 18:24:14 -0700

    Worked on scripts Added a better Grid Creater

---
+++ Tower Defense/Assets/Scripts/Button.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Button : MonoBehaviour {
+	private GridShow gridShow;
+	// Update is called once per frame
+	public bool buttonPress = false;
+	void Start()
+	{
+		GameObject gridObject = GameObject.Find ("GridBlock(Clone)");
+		if (gridObject != null)
+		{
+			Debug.Log ("Script Found");
+			gridShow = gridObject.GetComponent<GridShow> ();
+		}
+		if (gridObject == null)
+		{
+			Debug.Log ("Cannot find Grid Script");
+		}
+	}
+	void Update()
+	{
+		//for(int i = 0; i < Input.touchCount; i++)
+		//{
+		if (Input.GetMouseButtonDown (0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast (ray))
+			{
+				gridShow.isActive = true;
+			}
+
+		}
+		//}
+
+	}
+}
---
+++ Tower Defense/Assets/Scripts/GridShow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridShow : MonoBehaviour {
+	public bool isActive = false;
+	private Button button;
+	// Use this for initialization
+	void Start () {
+		GameObject buttonObject = GameObject.FindGameObjectWithTag ("Button");
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		if (isActive == true)
+		{
+			gameObject.GetComponent<Renderer> ().enabled = true;
+		}
+		else
+		{
+			gameObject.GetComponent<Renderer> ().enabled = false;
+		}
+	}
+}
